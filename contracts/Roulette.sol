39fa0998d5841d2425d4788ff370d5ab70d4a8f7
Callum McIntyre <callum.mcintyre@procensus.com>
2021-09-19 18:34:59 +0100

    Run prettier on contract

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -7,17 +7,17 @@ contract Roulette {
     uint8 private seed;
 
     mapping(uint8 => uint8) reds;
-    mapping(address => uint) public donations;
+    mapping(address => uint256) public donations;
     address[] public donationAddresses;
 
-    event BalanceChanged(uint newBalance);
-    event PlayerPayout(uint8 requestId, uint8 spin, uint winAmount);
+    event BalanceChanged(uint256 newBalance);
+    event PlayerPayout(uint8 requestId, uint8 spin, uint256 winAmount);
     event PlayerLost(uint8 requestId, uint8 spin);
 
     struct Bet {
         uint8 kind;
         uint8 param;
-        uint amount;
+        uint256 amount;
     }
 
     constructor() payable {
@@ -25,9 +25,28 @@ contract Roulette {
         emit BalanceChanged(msg.value);
 
         // From Wikipedia: https://en.wikipedia.org/wiki/Roulette
-        uint8[18] memory redsArray = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-        for (uint i=0; i<redsArray.length; i++) {
-            uint r = redsArray[i];
+        uint8[18] memory redsArray = [
+            1,
+            3,
+            5,
+            7,
+            9,
+            12,
+            14,
+            16,
+            18,
+            19,
+            21,
+            23,
+            25,
+            27,
+            30,
+            32,
+            34,
+            36
+        ];
+        for (uint256 i = 0; i < redsArray.length; i++) {
+            uint256 r = redsArray[i];
             reds[uint8(r)] = 1;
         }
     }
@@ -36,15 +55,17 @@ contract Roulette {
         validateBets(bets, msg.value);
 
         // pseudo-random with a seed
-        uint8 rouletteSpin = uint8((block.difficulty + block.timestamp + seed) % 37);
+        uint8 rouletteSpin = uint8(
+            (block.difficulty + block.timestamp + seed) % 37
+        );
         console.log("Spun the wheel! %d", rouletteSpin);
         seed = rouletteSpin;
 
-        uint payout = calculatePayout(bets, rouletteSpin);
+        uint256 payout = calculatePayout(bets, rouletteSpin);
 
-        if(payout > 0) {
+        if (payout > 0) {
             require(payout < address(this).balance, "Sorry, casino's bust!");
-            (bool success,) = (msg.sender).call{value: payout}("");
+            (bool success, ) = (msg.sender).call{value: payout}("");
             require(success, "Failed to pay out, sorry!");
             emit PlayerPayout(requestId, rouletteSpin, payout);
         } else {
@@ -53,87 +74,121 @@ contract Roulette {
         emit BalanceChanged(address(this).balance);
     }
 
-    function validateBets(Bet[] calldata bets, uint expectedTotal) pure public {
-        uint sum = 0;
-        for(uint i=0; i<bets.length; i++) {
+    function validateBets(Bet[] calldata bets, uint256 expectedTotal)
+        public
+        pure
+    {
+        uint256 sum = 0;
+        for (uint256 i = 0; i < bets.length; i++) {
             sum += bets[i].amount;
         }
-        require(sum == expectedTotal, "Input bet amounts do not add up to msg value");
+        require(
+            sum == expectedTotal,
+            "Input bet amounts do not add up to msg value"
+        );
     }
 
-    function calculatePayout(Bet[] calldata bets, uint8 rouletteSpin) view public returns (uint) {
-        uint payout = 0;
-        for(uint i=0; i<bets.length; i++) {
-            uint8 multiple = calculateReturnMultiple(rouletteSpin, bets[i].kind, bets[i].param);
+    function calculatePayout(Bet[] calldata bets, uint8 rouletteSpin)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 payout = 0;
+        for (uint256 i = 0; i < bets.length; i++) {
+            uint8 multiple = calculateReturnMultiple(
+                rouletteSpin,
+                bets[i].kind,
+                bets[i].param
+            );
             payout += (multiple * bets[i].amount);
         }
         return payout;
     }
 
-    function calculateReturnMultiple(uint8 rouletteSpin, uint8 betType, uint8 betParam) view public returns (uint8) {
+    function calculateReturnMultiple(
+        uint8 rouletteSpin,
+        uint8 betType,
+        uint8 betParam
+    ) public view returns (uint8) {
         // Outside bets
-        if(betType == 0) {
+        if (betType == 0) {
             // Black/Red
             bool win = rouletteSpin > 0 && reds[rouletteSpin] == betParam; // 0 wins if Black, 1 wins if Red
-            return win? 2 : 0;
-        } else if(betType == 1) {
+            return win ? 2 : 0;
+        } else if (betType == 1) {
             // Even/Odd
             bool win = rouletteSpin > 0 && rouletteSpin % 2 == betParam; // 0 wins if Even, 1 wins if Odd
-            return win? 2 : 0;
-        } else if(betType == 2) {
+            return win ? 2 : 0;
+        } else if (betType == 2) {
             // Low/High
-            if(rouletteSpin == 0) { return 0; }
-            if(rouletteSpin <= 18 && betParam == 0) { return 2; } // 0 wins if Low
-            if(rouletteSpin > 18 && betParam == 1) { return 2; } // 1 wins if High
+            if (rouletteSpin == 0) {
+                return 0;
+            }
+            if (rouletteSpin <= 18 && betParam == 0) {
+                return 2;
+            } // 0 wins if Low
+            if (rouletteSpin > 18 && betParam == 1) {
+                return 2;
+            } // 1 wins if High
             return 0;
-        } else if(betType == 3) {
+        } else if (betType == 3) {
             // One of 3 columns
             bool win = rouletteSpin > 0 && rouletteSpin % 3 == betParam; // 1 wins if col 1, 2 wins if col 2, 0 wins if col3
-            return win? 3 : 0;
-        } else if(betType == 4) {
+            return win ? 3 : 0;
+        } else if (betType == 4) {
             // Low/Medium/High third
-            if(rouletteSpin == 0) { return 0; }
-            if(rouletteSpin <= 12 && betParam == 0) { return 3; } // 0 wins if 1-12
-            if(rouletteSpin > 12 && rouletteSpin <= 24 && betParam == 1) { return 3; } // 1 wins if 13-24
-            if(rouletteSpin > 24 && rouletteSpin <= 36 && betParam == 2) { return 3; } // 2 wins if 25-36
+            if (rouletteSpin == 0) {
+                return 0;
+            }
+            if (rouletteSpin <= 12 && betParam == 0) {
+                return 3;
+            } // 0 wins if 1-12
+            if (rouletteSpin > 12 && rouletteSpin <= 24 && betParam == 1) {
+                return 3;
+            } // 1 wins if 13-24
+            if (rouletteSpin > 24 && rouletteSpin <= 36 && betParam == 2) {
+                return 3;
+            } // 2 wins if 25-36
             return 0;
         }
         // Inside bets
-        else if(betType == 100) {
+        else if (betType == 100) {
             // Straight up bet
             bool win = rouletteSpin == betParam;
-            return win? 36 : 0; 
-        } else if(betType == 101) {
+            return win ? 36 : 0;
+        } else if (betType == 101) {
             // Horizontal split
             // Param assumed to be on the left, so win for param or the number to its right (+1)
             bool win = rouletteSpin == betParam || rouletteSpin == betParam + 1;
-            return win? 18 : 0;
-        } else if(betType == 102) {
+            return win ? 18 : 0;
+        } else if (betType == 102) {
             // Vertical split
             // Param assumed to be top of the split, so win for param or the number below (+3)
             bool win = rouletteSpin == betParam || rouletteSpin == betParam + 3;
-            return win? 18 : 0;
-        } else if(betType == 103) {
+            return win ? 18 : 0;
+        } else if (betType == 103) {
             // Street
             // Param assumed to be left number in the row, so win for param, param+1, param+3
             bool win = rouletteSpin >= betParam && rouletteSpin <= betParam + 2;
-            return win? 12 : 0;
-        } else if(betType == 104) {
+            return win ? 12 : 0;
+        } else if (betType == 104) {
             // Corner
             // Param assumed to be top left of the corner, so bet covers the param, the number to its right (+1), the number below (+3), the number below right (+4)
-            bool win = rouletteSpin == betParam || rouletteSpin == betParam + 1 || rouletteSpin == betParam + 3 || rouletteSpin == betParam + 4;
-            return win? 9 : 0;
-        } else if(betType == 105) {
+            bool win = rouletteSpin == betParam ||
+                rouletteSpin == betParam + 1 ||
+                rouletteSpin == betParam + 3 ||
+                rouletteSpin == betParam + 4;
+            return win ? 9 : 0;
+        } else if (betType == 105) {
             // Line (2 rows)
             // Param assumed to be the top left of the two rows, so win for param to param + 5 (eg 1-6)
             bool win = rouletteSpin >= betParam && rouletteSpin <= betParam + 5;
-            return win? 6 : 0;
-        } else if(betType == 106) {
+            return win ? 6 : 0;
+        } else if (betType == 106) {
             // Basket (0,1,2,3)
             bool win = rouletteSpin <= 3;
-            return win? 7 : 0;
-        }
-        else {
+            return win ? 7 : 0;
+        } else {
             revert("Bet type not implemented, sorry!");
         }
     }
@@ -141,7 +196,7 @@ contract Roulette {
     function donate() public payable {
         require(msg.value > 0, "Sorry but you can't donate nothing!");
 
-        if(donations[msg.sender] == 0) {
+        if (donations[msg.sender] == 0) {
             donationAddresses.push(msg.sender);
         }
 
@@ -149,7 +204,7 @@ contract Roulette {
         emit BalanceChanged(address(this).balance);
     }
 
-    function getDonationAddresses() view public returns (address[] memory) {
+    function getDonationAddresses() public view returns (address[] memory) {
         return donationAddresses;
     }
 }
